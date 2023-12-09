8ceedbcc3c3255f759c33aa40f029be78267902b
Atharva Naik <atharvashrik@umass.edu>
2023-12-09 18:27:17 -0500

    Added redirection to book appointment page from patient dashboard

--- client/patient-tracker/src/components/dashboard/patient_dashboard.js
+++ client/patient-tracker/src/components/dashboard/patient_dashboard.js
@@ -3,7 +3,12 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faUser} from '@fortawesome/free-solid-svg-icons'
 import "./doc_dash.css";
 
+
 const Pat_Dash = ({ onCreateAppointmentClick, onAppointmentsClick, onTestResultsClick, onProfileClick }) => {
+  const handleSubmit = (e,page) => {
+    e.preventDefault();
+    window.location.replace(page)
+  }
   return (
     <div>
       <nav className="navbar">
@@ -11,7 +16,7 @@ const Pat_Dash = ({ onCreateAppointmentClick, onAppointmentsClick, onTestResults
       </nav>
       <div className="main-buttons">
         <button onClick={onAppointmentsClick}>Check My Scheduled Appointments</button>
-        <button onClick={onCreateAppointmentClick}>Schedule Appointment</button>
+        <button onClick={(e) => {handleSubmit(e,"/create_appointment")}}>Schedule Appointment</button>
         <button onClick={onTestResultsClick}>Check My Test Results</button>
       </div>
       
