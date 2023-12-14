d7cf34fca978fa1d44d6c60c62e79e45638f300c
Atharva Naik <atharvashrik@umass.edu>
2023-12-14 08:02:53 -0500

    Implemented api to schedule an appointment along with checking for clashes and doctor availablity

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import glob
 import os
 
 import flask
@@ -7,7 +8,6 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app, origins="http://localhost:3000")
-
 
 def get_db_connection():
     print("flask")
@@ -131,9 +131,10 @@ def login():
     email = data['email']
     entered_password = data['password']
     user_type = data["type"]
+    print(entered_password)
     try:
         if "doctor" in user_type:
-            cur.execute('''SELECT emp_password FROM employee WHERE emp_email = %s''', (email,))
+            cur.execute('''SELECT emp_password,  FROM employee WHERE emp_email = %s''', (email,))
             actual_password = cur.fetchall()
             if actual_password[0][0] == entered_password:
                 return "Login successful", 200
@@ -142,11 +143,11 @@ def login():
 
         elif "patient" in user_type:
             print("in pabyg")
-            cur.execute('''SELECT patient_password FROM patient WHERE patient_email = %s''', (email,))
+            cur.execute('''SELECT patient_password,patient_name FROM patient WHERE patient_email = %s''', (email,))
             actual_password = cur.fetchall()
-            print("actual_password", actual_password)
+            print("actual_password", actual_password[0][0])
             if actual_password[0][0] == entered_password:
-                return "Login successful", 200
+                return {"name": actual_password[0][1]}, 200
             else:
                 return "Authentication failed", 400
         else:
@@ -174,6 +175,72 @@ def get_appointment():
         WHERE sched_start_time ::date 
         = %s''', (date,))
         appointments = cur.fetchall()
+        return appointments, 200
+
+    except:
+        return "Error", 400
+
+    finally:
+        # close the cursor and connection
+        cur.close()
+        conn.close()
+
+@app.route('/schedule_appointment', methods=['POST', 'OPTIONS'])
+@cross_origin()
+def set_appointment():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    try:
+        data = request.get_json()
+        doc_name = data['doctor']
+        patient_name = data['patient']
+        appointmentDate = data['date']
+        appointmentTime = data['time']
+
+        print(doc_name,patient_name,appointmentDate,appointmentTime)
+
+        cur.execute("SELECT employee_id FROM employee where employee_name= %s", (doc_name,))
+        doc_id = cur.fetchall()
+        print(doc_id[0][0])
+        cur.execute("SELECT patient_id FROM patient where patient_name= %s", (patient_name,))
+        pat_id = cur.fetchall()
+        print(pat_id[0][0])
+
+        datetime = appointmentDate+" "+appointmentTime
+
+        cur.execute('''SELECT * FROM scheduled_clinic_hours WHERE doc_id=%s and sched_start_time=%s''',(doc_id[0][0], datetime))
+        exists = cur.fetchall()
+        if exists:
+            return "This time slot is not available", 400
+
+        patient = cur.execute(
+            '''INSERT INTO scheduled_clinic_hours
+            (doc_id, patient_id, sched_start_time, department_id) VALUES (%s, %s, %s, 1)''',
+            (doc_id[0][0], pat_id[0][0], datetime))
+
+        # commit the changes
+        conn.commit()
+        # close the cursor and connection
+        cur.close()
+        conn.close()
+        return "Appointment Scheduled Successfully", 200
+    except:
+        # close the cursor and connection
+        cur.close()
+        conn.close()
+        return "Error", 400
+
+
+@app.route('/get_docs', methods=['GET', 'OPTIONS'])
+@cross_origin()
+def get_docs():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    date = request.args.get('date')
+    try:
+        cur.execute('''SELECT employee_name FROM employee where job_id=1''')
+        appointments = cur.fetchall()
+        print(appointments)
         return appointments, 200
 
     except:
---
+++ client/patient-tracker/src/components/manage-appointment/schedule_appointment.js
@@ -0,0 +1,115 @@
+import React, { useState } from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faUser} from '@fortawesome/free-solid-svg-icons'
+import "./schedule_appointment.css"
+import getPatientName from '../login/login.js'
+import { ReactSession } from 'react-client-session';
+
+const AppointmentForm = ({ onSubmit }) => {
+  const [doctorName, setDoctorName] = useState('');
+  const [doctorNames, setDoctorNames] = useState([]);
+  const [appointmentDate, setAppointmentDate] = useState('');
+  const [appointmentTime, setAppointmentTime] = useState('');
+  const patient = ReactSession.get("patient_name");
+  const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:5000/schedule_appointment",{
+                method: "POST",
+                mode : "cors",
+                headers: {
+                  'Content-Type' : 'application/json',
+                  'Access-Control-Allow-Credentials': 'true',
+                  'Access-Control-Allow-Origin':'http://127.0.0.1:5000',
+                  'Access-Control-Allow-Methods':'POST,GET,OPTIONS',
+                  'Access-Control-Allow-Headers': '*'
+                },
+                body: JSON.stringify({"doctor":doctorName,"date":appointmentDate,"time":appointmentTime,"patient":patient})
+              }).catch(error => console.log(error))
+              if (response.ok) {
+                alert("Successfully Scheduled!!!");
+                window.location.replace("/patient_dashboard");
+              }
+
+              else {
+                alert("This time slot is not available");
+              }
+  };
+
+window.onload = async function() {
+    const app_info = await fetch("http://localhost:5000/get_docs",{
+                method: 'GET',
+                mode : "cors",
+                headers: {
+                  'Access-Control-Allow-Credentials': 'true',
+                  'Access-Control-Allow-Origin':'http://127.0.0.1:5000',
+                  'Access-Control-Allow-Methods':'GET,OPTIONS'
+                },
+              }
+            ).then((response) => response.json())
+            .then((data) => data);
+    setDoctorNames(app_info);
+
+}
+
+  return (
+    <div className="formbold-main-wrapper">
+      <div className="formbold-form-wrapper">
+        <form onSubmit={handleSubmit}>
+          <div className="formbold-mb-5">
+            <label htmlFor="doctor" className="formbold-form-label"> Select Doctor </label>
+            <select
+              id="doctor"
+              value={doctorName}
+              className="formbold-form-input"
+              onChange={(e) => setDoctorName(e.target.value)}
+            >
+              <option value="">Select Doctor</option>
+              {doctorNames.map((doctor, index) => (
+                <option key={index} value={doctor}>
+                  {doctor}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="formbold-mb-5">
+            <label htmlFor="date" className="formbold-form-label"> Appointment Date </label>
+            <input
+              type="date"
+              value={appointmentDate}
+              onChange={(e) => setAppointmentDate(e.target.value)}
+            />
+          </div>
+          <div className="formbold-mb-5">
+            <label htmlFor="time" className="formbold-form-label"> Appointment Time </label>
+            <input
+              type="time"
+              value={appointmentTime}
+              onChange={(e) => setAppointmentTime(e.target.value)}
+            />
+          </div>
+          <button type="submit">Book Appointment</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+const AppointmentBookingPage = () => {
+  const handleFormSubmit = (formData) => {
+    // send the data to your backend
+    console.log('Form submitted:', formData);
+    // save the appointment in database
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <button class="right"><FontAwesomeIcon icon={faUser}></FontAwesomeIcon> Profile</button>
+      </nav>
+      <h1>Book Appointment</h1>
+      <AppointmentForm onSubmit={handleFormSubmit} />
+    </div>
+  );
+};
+
+export default AppointmentBookingPage;
